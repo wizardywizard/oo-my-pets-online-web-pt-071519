5854c7cd63b1a70f2c92afe67e3e995c99449514
David Little <littledavid175@gmail.com>
2019-08-06 00:09:21 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+   attr_reader :name
+  attr_accessor :mood, :owner
+
+@@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+  end
+  
+  def self.all
+    @@all
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+   attr_reader :name
+  attr_accessor :mood, :owner
+
+@@all = []
+
+  def initialize(name, mood)
+    @name = name
+    @mood = "nervous"
+  end
+  
+  def self.all
+    @@all
+  end
 end
